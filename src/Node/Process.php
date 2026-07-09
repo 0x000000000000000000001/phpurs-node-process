a97702828d5624c8ad83351fedba3ac7c5f79d21
0x1 <kevin.francart3@gmail.com>
2026-07-09 22:11:32 +0200

    Add PHP FFI for Node.Process

---
+++ src/Node/Process.php
@@ -0,0 +1,32 @@
+<?php
+
+$exports['argv'] = function() {
+    global $argv;
+    return $argv ?? [];
+};
+
+$exports['setEnv'] = function($key) {
+    return function($val) {
+        return function() use ($key, $val) {
+            putenv("$key=$val");
+        };
+    };
+};
+
+$exports['lookupEnv'] = function($key) {
+    return function() use ($key) {
+        $val = getenv($key);
+        return $val !== false ? $val : null;
+    };
+};
+
+$exports['stdout'] = defined('STDOUT') ? STDOUT : fopen('php://stdout', 'w');
+$exports['stderr'] = defined('STDERR') ? STDERR : fopen('php://stderr', 'w');
+
+$exports['exitImpl'] = function($code) {
+    return function() use ($code) {
+        exit($code);
+    };
+};
+
+return $exports;
